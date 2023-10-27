ebdd9f8dffa64455c2d96ae4385dda721669b082
dukeossuna <andregsistemas@outlook.com>
2023-10-27 10:12:55 -0300

    Arquivos- Desenvolvendo o temporizador

--- script/main.js
+++ script/main.js
@@ -4,10 +4,25 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFoco = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+musicaFoco.addEventListener('change', function(){
+    if(musica.paused){
+        musica.play();
+    }else {
+        musica.pause();
+    }
+});
 
 function alterarContexto(contexto){
     html.setAttribute('data-contexto' , contexto);
     banner.setAttribute('src',`/imagens/${contexto}.png`);
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active');
+    })
     
     switch(contexto){
         case "foco":
@@ -29,14 +44,17 @@ function alterarContexto(contexto){
 //Usei neste caso uma função anonima, mas podemos usaras arrows Function
 focoBt.addEventListener('click', function() {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', function() {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 //Neste ultimo caso eu utilzei a Arrow Function
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
 
